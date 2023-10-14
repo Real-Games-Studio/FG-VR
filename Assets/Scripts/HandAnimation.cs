5f95e5bb9c24e425b52e7bf408ddc8be1715ee8c
MatheusSMA <matheusjut@gmail.com>
2023-10-14 02:31:31 -0300

    Touch thumbs and trigger now working with animation

--- Assets/Scripts/HandAnimation.cs
+++ Assets/Scripts/HandAnimation.cs
@@ -3,27 +3,89 @@ using UnityEngine;
 using UnityEngine.XR;
 using TMPro;
 using System;
+using System.Xml.Linq;
+
+enum Controller
+{
+    LeftHand,
+    RightHand
+}
 
 public class HandAnimation : MonoBehaviour
 {
+    public const float INPUT_RATE_CHANGE = 15.0f;
+    private float pointBlend, thumbBlend;
+    private Animator myAnimator;
+    private List<InputFeatureUsage> inputFeatures = new List<InputFeatureUsage>();
+    private Dictionary<Controller, XRNode> controllerType = new Dictionary<Controller, XRNode>
+    {
+        {Controller.LeftHand , XRNode.LeftHand },
+        {Controller.RightHand , XRNode.RightHand }
+    };
+    [SerializeField] private Controller sideController;
 
-    [SerializeField] private TextMeshProUGUI text;
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
 
-    [SerializeField] private int index;
-    List<InputFeatureUsage> inputFeatures = new List<InputFeatureUsage>();
+    private void Update()
+    {
+        UpdateAnimStates();
+    }
 
 
-    // Update is called once per frame
 
-    void Update()
+    private void SetGripAnim(InputDevice controller)
+    {
+        controller.TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out float value);
+        myAnimator.SetFloat("Grip", value);
+    }
+
+    private void UpdateAnimStates()
     {
 
-        InputDevice leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
-        leftController.TryGetFeatureUsages(inputFeatures);
+        InputDevice vrController = InputDevices.GetDeviceAtXRNode(controllerType[sideController]);
+        vrController.TryGetFeatureUsages(inputFeatures);
+
+        // Grip
+        SetGripAnim(vrController);
 
         //Avoid the exception of wrong object type comparassion. (because inputfeatures list return bool and Vector2)
-        if (inputFeatures[index].type == typeof(bool))
-            if (leftController.TryGetFeatureValue(inputFeatures[index].As<bool>(), out bool thumb))
-                text.text = $"{index} ThumbTouch: " + thumb;
+        if (inputFeatures[5].type == typeof(bool))
+        {
+            // Thumbs up
+            vrController.TryGetFeatureValue(inputFeatures[5].As<bool>(), out bool thumb);
+            thumbBlend = InputValueRateChange(!thumb, thumbBlend);
+            myAnimator.SetLayerWeight(1, thumbBlend);
+
+
+            // Point
+            vrController.TryGetFeatureValue(inputFeatures[10].As<bool>(), out bool point);
+            pointBlend = InputValueRateChange(!point, pointBlend);
+            myAnimator.SetLayerWeight(2, pointBlend);
+        }
     }
+
+    private float InputValueRateChange(bool isDown, float value)
+    {
+        float rateDelta = Time.deltaTime * INPUT_RATE_CHANGE;
+        float sign = isDown ? 1.0f : -1.0f;
+        return Mathf.Clamp01(value + rateDelta * sign);
+    }
+
+
+    // void Update()
+    // {
+
+    //     InputDevice leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+    //     leftController.TryGetFeatureUsages(inputFeatures);
+
+    //     //Avoid the exception of wrong object type comparassion. (because inputfeatures list return bool and Vector2)
+    //     if (inputFeatures[index].type == typeof(bool))
+    //     {
+    //         leftController.TryGetFeatureValue(inputFeatures[index].As<bool>(), out bool thumb);
+    //         text.text = $"{index} ThumbTouch: " + thumb;
+    //     }
+    // }
 }
