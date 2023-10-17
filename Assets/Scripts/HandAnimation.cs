95c53398883bcbd663b318401d02fa8f26fe1ea5
MatheusSMA <matheusjut@gmail.com>
2023-10-16 21:48:47 -0300

    Adjusts on hand animation code

--- Assets/Scripts/HandAnimation.cs
+++ Assets/Scripts/HandAnimation.cs
@@ -4,6 +4,8 @@ using UnityEngine.XR;
 using TMPro;
 using System;
 using System.Xml.Linq;
+using UnityEngine.InputSystem.XR;
+using UnityEditor.XR.OpenXR.Features;
 
 enum Controller
 {
@@ -13,7 +15,7 @@ enum Controller
 
 public class HandAnimation : MonoBehaviour
 {
-    public const float INPUT_RATE_CHANGE = 15.0f;
+    public const float INPUT_RATE_CHANGE = 20.0f;
     private float pointBlend, thumbBlend;
     private Animator myAnimator;
     private List<InputFeatureUsage> inputFeatures = new List<InputFeatureUsage>();
@@ -34,45 +36,37 @@ public class HandAnimation : MonoBehaviour
         UpdateAnimStates();
     }
 
-
-
-    private void SetGripAnim(InputDevice controller)
-    {
-        controller.TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out float value);
-        myAnimator.SetFloat("Grip", value);
-    }
-
     private void UpdateAnimStates()
     {
         InputDevice vrController = InputDevices.GetDeviceAtXRNode(controllerType[sideController]);
         vrController.TryGetFeatureUsages(inputFeatures);
 
-        // Grip
-        SetGripAnim(vrController);
-
-        //Avoid the exception of wrong object type comparassion. (because inputfeatures list return bool and Vector2)
-        if (inputFeatures[5].type == typeof(bool))
+        //Run only with the controller device is active
+        if (vrController.TryGetFeatureValue(inputFeatures[20].As<bool>(), out bool isActive) && isActive)
         {
+            // Grip
+            SetGripAnim(vrController);
 
-            // Thumbs up
-            bool thumb;
-            if (vrController.TryGetFeatureValue(inputFeatures[5].As<bool>(), out thumb) || vrController.TryGetFeatureValue(inputFeatures[7].As<bool>(), out thumb) || vrController.TryGetFeatureValue(inputFeatures[12].As<bool>(), out thumb) || vrController.TryGetFeatureValue(inputFeatures[13].As<bool>(), out thumb))
+            //Avoid the exception of wrong object type compare. (because inputfeatures list return bool and Vector2)
+            if (inputFeatures[5].type == typeof(bool))
             {
+                // Thumbs up                                        
+                vrController.TryGetFeatureValue(inputFeatures[5].As<bool>(), out bool thumb);
                 thumbBlend = InputValueRateChange(!thumb, thumbBlend);
                 myAnimator.SetLayerWeight(1, thumbBlend);
-                Debug.Log("teste");
-            }
 
-            // if (vrController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryTouch, out bool a)) { }            
-
-            // Point
-            if (vrController.TryGetFeatureValue(inputFeatures[10].As<bool>(), out bool point) && point) // "&& point" to call only when pressed
-            {
+                // Point
+                vrController.TryGetFeatureValue(inputFeatures[10].As<bool>(), out bool point); // "&& point" to call only when pressed            
                 pointBlend = InputValueRateChange(!point, pointBlend);
                 myAnimator.SetLayerWeight(2, pointBlend);
-                Debug.Log("teste 2");
             }
         }
+    }
+
+    private void SetGripAnim(InputDevice controller)
+    {
+        controller.TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out float value);
+        myAnimator.SetFloat("Grip", value);
     }
 
     private float InputValueRateChange(bool isDown, float value)
